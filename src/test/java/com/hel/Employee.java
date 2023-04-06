675ee0dbaeae409921135eb17af0139a8ca74dbc
Suryaprabha-S <prabhasuryas97@gmail.com>
2023-04-06 11:36:50 +0530

    surya prabha

---
+++ src/test/java/com/hel/Employee.java
@@ -0,0 +1,14 @@
+package com.hel;
+
+public class Employee {
+private void emp1() {
+	System.out.println(2);
+}
+private void emp2() {
+	System.out.println(6);
+}
+private void emp3() {
+	System.out.println(9);
+	
+}
+}
