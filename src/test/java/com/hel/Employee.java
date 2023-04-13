35ef31a5d97fa15f5eb78cac8d49498a75a7089a
ArthiKesav <arthinirmala16@gmail.com>
2023-04-13 21:50:59 +0530

    added 3 methods

--- src/test/java/com/hel/Employee.java
+++ src/test/java/com/hel/Employee.java
@@ -9,6 +9,14 @@ private void emp2() {
 }
 private void emp3() {
 	System.out.println(9);
-	
+}
+private void tamilNadu() {
+	System.out.println("tamil");
+}
+private void kerala() {
+	System.out.println("malayalam");
+}
+private void andra() {
+	System.out.println("telugu");
 }
 }
